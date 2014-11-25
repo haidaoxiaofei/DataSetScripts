0c1b9bfd6d65827fc09690e4ff7bb2341060f88d
haidaoxiaofei <haidaoxiaofei@gmail.com>
2014-11-25 18:55:29 +0800

    initial

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#This script can remove the duplicated lines of the files in the indicated directory
+cd T-Drive
+for filename in $(ls -1)
+do 
+	cat $filename | uniq > tmp
+	mv tmp $filename
+done
